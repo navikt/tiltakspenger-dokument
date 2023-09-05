319621d27cc3f793839a0b62a44e78ed70643d50
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2023-09-05 10:30:20 +0200

    Helsesjekk

--- src/main/kotlin/no/nav/tiltakspenger/dokument/Application.kt
+++ src/main/kotlin/no/nav/tiltakspenger/dokument/Application.kt
@@ -1,6 +1,13 @@
 package no.nav.tiltakspenger.dokument
 
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStarted
+import io.ktor.server.application.ApplicationStopped
+import io.ktor.server.application.install
+import io.ktor.server.routing.routing
+import io.ktor.server.plugins.callloging.CallLogging
 import mu.KotlinLogging
+import no.nav.tiltakspenger.dokument.health.healthRoutes
 
 fun main(args: Array<String>) {
     System.setProperty("logback.configurationFile", "egenLogback.xml")
@@ -13,7 +20,30 @@ fun main(args: Array<String>) {
         securelog.error(e) { e.message }
     }
 
-    log.info { "starting server" }
-
     io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    System.setProperty("logback.configurationFile", "egenLogback.xml")
+
+    val log = KotlinLogging.logger {}
+    val securelog = KotlinLogging.logger("tjenestekall")
+
+    Thread.setDefaultUncaughtExceptionHandler { _, e ->
+        log.error { "Uncaught exception logget i securelog" }
+        securelog.error(e) { e.message }
+    }
+
+    install(CallLogging)
+
+    routing {
+        healthRoutes()
+    }
+
+    environment.monitor.subscribe(ApplicationStarted) {
+        log.info { "Starter server" }
+    }
+    environment.monitor.subscribe(ApplicationStopped) {
+        log.info { "Stopper server" }
+    }
 }
